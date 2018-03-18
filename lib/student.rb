dc19d5e28bf5dd7d4e660e6f1e4b3a367e6d1656
adam n <adam.nawroc@gmail.com>
2018-03-18 13:21:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student
-  attr_accessor :id, :name, :grade, :length
+  attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     new_student = self.new  # self.new is the same as running Song.new
     new_student.id = row[0]
     new_student.name =  row[1]
-    new_student.length = row[2]
+    new_student.grade = row[2]
     new_student  # return the newly created instance
   end
 
