89f227dd5ceab2a4b066471b502dad6bbe7889ee
adam n <adam.nawroc@gmail.com>
2018-03-18 13:29:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,10 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10
+
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
