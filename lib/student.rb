ae18168f29cf9a3ebbe06f57841f18384f5d6828
adam n <adam.nawroc@gmail.com>
2018-03-18 13:35:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,9 +96,9 @@ class Student
       WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
 
   def save
